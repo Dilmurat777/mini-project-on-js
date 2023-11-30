d79fe474e5f4ef4f2241dee271b9704a27d886cc
Dilmurat777 <mrdilmurat777@gmail.com>
2023-11-30 13:29:44 +0600

    added burger menu

--- sticky-navigation-bar-on-scroll/app.js
+++ sticky-navigation-bar-on-scroll/app.js
@@ -1,6 +1,9 @@
 const marker = document.getElementById('marker');
 const item = document.querySelectorAll('nav a');
 const header = document.querySelector('header');
+const menuBtn = document.querySelector('.menu__burger-btn');
+// const menuBtnClose = document.querySelector('.menu__burger-close');
+const menuNav = document.querySelector('.menu');
 
 window.addEventListener('scroll', function() {
 	header.classList.toggle('sticky', window.scrollY > 0)
@@ -16,3 +19,11 @@ item.forEach(link => {
 		indicator(e.target)
 	})
 })
+
+menuBtn.addEventListener('click', function() {
+	menuNav.classList.toggle('open')
+	
+})
+// menuBtnClose.addEventListener('click', function() {
+// 	menuNav.classList.remove('open')
+// })
